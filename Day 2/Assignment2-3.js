b5ae961278aabfcae7e252c8616dc1a9aea39540
naveen4923 <68223871+naveen4923@users.noreply.github.com>
2020-07-16 00:21:24 -0700

    Create Assignment2-3.js

---
+++ Day 2/Assignment2-3.js
@@ -0,0 +1,10 @@
+// Question 3 : Ask the user if he is 21+ and the value "Can drink" , " Cannot drink"
+
+let age = confirm("Are your 21+ if yes click ok");
+if(age){
+    console.log("Can drink");
+}
+
+else{
+    console.log("Cannot drink");
+}
