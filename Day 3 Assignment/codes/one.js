060e5504108b54f4efa27efbe8fcfad8aa009f67
naveen4923 <68223871+naveen4923@users.noreply.github.com>
2020-07-17 00:43:30 -0700

    Update one.js

--- Day 3 Assignment/codes/one.js
+++ Day 3 Assignment/codes/one.js
@@ -1,9 +1,19 @@
 console.log("Hello");
 //Question 1
-let num = Number(prompt("Input Any Number"));
-  console.log("The Number Entered is",+num+ "and")
-if (num % 2==0) {
-    console.log("Number is Even")
-} else{
-    console.log("Number is Odd")
+let num = +prompt("Enter a Number", '0');
+
+function check(numbers) {
+    let checker ;
+    if (num % 2 == 0){
+        checker = 'even';
+        return checker;
+    }
+    else{
+        checker = 'odd';
+        return checker;
+    }
+    
 }
+let variable = check(num);
+
+console.log(`The Number entered is ${num} and Number is ${variable}`);
