cc7981669c4359d6d58f077aac7ae01c3ee44195
naveen4923 <68223871+naveen4923@users.noreply.github.com>
2020-07-17 00:34:57 -0700

    Update three.js

--- Day 3 Assignment/codes/three.js
+++ Day 3 Assignment/codes/three.js
@@ -14,19 +14,19 @@ let marks = +prompt();
  }
 //using switch or ternary operator
 
-//using ternary operator
-console.log(marks);
-console.log(marks == 50? 'marks is 50 and grade is B':'marks is less and grade is c' );
-
 //using switch
 console.log(marks);
 switch (marks) {
-    case 50:
-        console.log("marks is 50 and grade is B");
-        break;
-    case 40:  
-         console.log("marks are less and grade is c");  
-   
+    case marks:
+        if (marks>=70&&marks<=100){
+        console.log(`marks is  ${marks}  and grade is A+`);
+        break;}
+
+    case marks :  
+    if (marks>=50&&marks<=70) {
+    console.log(`marks is  ${marks} and grade is B`);
+    break;
+    }
     default:
         console.log("fail");
         break;
